a0f1fde5d805b58bab9254c8fd52dc41032b3655
xavitooo <a21xavcabmil@inspedralbes.cat>
2023-10-23 12:55:31 +0200

    json productes fet

---
+++ index.js
@@ -0,0 +1,23 @@
+//Definim totes les constants que necessita el servidor per operar
+const express = require('express');
+const cors = require("cors");
+const fs = require('fs');
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, function () {
+    console.log("SERVER RUNNNIG");
+  });
+
+  {
+    "productes": [
+      {
+        "id":
+        "categoria":
+        "nom": 
+        "preu":
+        "url_imatge":
+      }
+    ]
+  }
+  
