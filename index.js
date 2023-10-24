0bc4a84602f7fe0dbc9e200b057c3b9d516aaadf
xavitooo <a21xavcabmil@inspedralbes.cat>
2023-10-24 10:27:32 +0200

    getProductes primera petició funciona

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
@@ -86,7 +86,7 @@ conexion.connect(function (error) { //Creo la conexión
 
 app.post("/postProductes", (req, res) => {
 
-
+  
 
 })
 
