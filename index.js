0f2c67a61ea745153d5f19b97a609712307c8394
xavitooo <a21xavcabmil@inspedralbes.cat>
2023-10-31 10:58:38 +0100

    diari

--- index.js
+++ index.js
@@ -391,3 +391,33 @@ app.put("/estatComanda", async (req, res) => {
     res.status(500).json({ message: "Error en aprovar la comanda" });
   }
 });
+
+//Pagar
+app.post("/pagar", async (req, res) => {
+
+  const { comandaId, nuevoEstado } = req.body;
+
+  if (!comandaId || !nuevoEstado) {
+    return res.status(400).json({ error: "Falten dades obligatòries" });
+  }
+
+  try {
+    // Verifica si la comanda con el ID proporcionado existe en la base de datos
+    const comandaExistente = await executeQuery("SELECT * FROM comanda WHERE id = ?", [comandaId]);
+
+    if (!comandaExistente.length) {
+      return res.status(404).json({ error: "Comanda no trobada" });
+    }
+
+    // Actualiza el estado de la comanda al nuevo estado proporcionado en el cuerpo
+    await executeQuery("UPDATE comanda SET estat = ? WHERE id = ?", [nuevoEstado, comandaId]);
+
+    io.emit("comandaActualitzada", { comandaId, nuevoEstado });
+
+    res.json({ message: "Comanda aprovada amb èxit" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error en aprovar la comanda" });
+  }
+
+})
