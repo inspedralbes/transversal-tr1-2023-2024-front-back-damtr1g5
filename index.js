1d2879ccbc26de9f489eac311ad6fbb3d8f91f9b
AndresOreVel <121023104+AndresOreVel@users.noreply.github.com>
2023-11-03 10:45:43 +0100

    Eliminado el mutlter

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var session = require('express-session');
 const multer = require('multer')
 const cors = require("cors");
 const fs = require('fs');
-const multer = require('multer')
 const app = express();
 const { createServer } = require('http');
 const { Server } = require('socket.io');
@@ -24,27 +23,6 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './imatges_productes');
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
-
-const upload = multer({ storage: storage });
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './imatges_productes');
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
-
-const upload = multer({ storage: storage });
 
 var conexion = null; //Se usa en el método de getEstadístiques
 
