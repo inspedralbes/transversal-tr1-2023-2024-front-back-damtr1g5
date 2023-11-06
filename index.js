a20ba32362966c119368af59d4c99b126342105a
xavitooo <a21xavcabmil@inspedralbes.cat>
2023-11-06 12:32:13 +0100

    linies cors comentades

--- index.js
+++ index.js
@@ -95,13 +95,13 @@ app.use(express.json());
 app.use(cors());
 
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
-})
+})*/
 
 // Funció que executa una consulta SQL a la base de dades i manipula la conexió. ES FA AMB UNA PROMISE
 function executeQuery(query, params = []) {
